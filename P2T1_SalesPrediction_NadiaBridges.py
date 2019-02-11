80ec83eb8060b898658b34505822d67aef9853b7
bridgesn5861 <47535433+bridgesn5861@users.noreply.github.com>
2019-02-11 12:32:28 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_NadiaBridges.py
@@ -0,0 +1,13 @@
+# Pounds to Kilograms.
+# 2/11/2019
+# CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Nadia Bridges
+
+# Pounds
+pounds = float(input(" Enter the number of pounds: "))
+
+# kg= lb/2.2046
+kilograms = pounds/2.2046
+
+# Diplay Kilograms
+print( " equals ", kilograms)
